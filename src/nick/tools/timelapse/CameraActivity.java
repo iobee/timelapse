9f39b005ee37f889c1fafbb2b32d17c21ce32b85
iobee <iobee@sina.com>
2013-02-04 23:11:54 +0800

    add video capture funtion.

--- src/nick/tools/timelapse/CameraActivity.java
+++ src/nick/tools/timelapse/CameraActivity.java
@@ -4,22 +4,32 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import nick.tools.timelapse.ui.CameraPreview;
 import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 public class CameraActivity extends Activity {
+	
+	private static final int MEDIA_TYPE_IMAGE = 1;
+	private static final int MEDIA_TYPE_VIDEO = 2;
 
 	protected static final String TAG = null;
 	private Camera mCamera;
 	private CameraPreview mPreview;
+	private MediaRecorder mMediaRecorder;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -56,7 +66,14 @@ public class CameraActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
-		mCamera.takePicture(null, null, mPicture);
+		//mCamera.takePicture(null, null, mPicture);
+		releaseCamera();
+		
+		if(prepareVideoRecorder()){
+			mMediaRecorder.start();
+		} else {
+			releaseMediaRecorder();
+		}
 		return true;
 	}
 
@@ -64,6 +81,7 @@ public class CameraActivity extends Activity {
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
+		releaseMediaRecorder();
 		releaseCamera();
 	}
 
@@ -97,4 +115,87 @@ public class CameraActivity extends Activity {
 			}
 		}
 	};
+	
+	/** Create a file Uri for saving an image or video */
+	private static Uri getOutputMediaFileUri(int type){
+	      return Uri.fromFile(getOutputMediaFile(type));
+	}
+	
+	private boolean prepareVideoRecorder(){
+		mCamera = getCameraInstance();
+		mMediaRecorder = new MediaRecorder();
+		
+		mCamera.unlock();
+		mMediaRecorder.setCamera(mCamera);
+		
+		mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+		mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+		
+		mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+		
+		mMediaRecorder.setOutputFile("/sdcard/camera.mp4");
+
+	    // Step 5: Set the preview output
+	    mMediaRecorder.setPreviewDisplay(mPreview.getHolder().getSurface());
+	    
+	    mMediaRecorder.setMaxDuration(60000); // Set max duration 60 sec.
+        mMediaRecorder.setMaxFileSize(5000000); // Set max file size 5M
+
+	    // Step 6: Prepare configured MediaRecorder
+	    try {
+	        mMediaRecorder.prepare();
+	    } catch (IllegalStateException e) {
+	        Log.d(TAG, "IllegalStateException preparing MediaRecorder: " + e.getMessage());
+	        releaseMediaRecorder();
+	        return false;
+	    } catch (IOException e) {
+	        Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
+	        releaseMediaRecorder();
+	        return false;
+	    }
+	    return true;
+	}
+	
+	private void releaseMediaRecorder(){
+        if (mMediaRecorder != null) {
+            mMediaRecorder.reset();   // clear recorder configuration
+            mMediaRecorder.release(); // release the recorder object
+            mMediaRecorder = null;
+            mCamera.lock();           // lock camera for later use
+        }
+    }
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type){
+	    // To be safe, you should check that the SDCard is mounted
+	    // using Environment.getExternalStorageState() before doing this.
+
+	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+	              Environment.DIRECTORY_PICTURES), "MyCameraApp");
+	    // This location works best if you want the created images to be shared
+	    // between applications and persist after your app has been uninstalled.
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()){
+	        if (! mediaStorageDir.mkdirs()){
+	            Log.d("MyCameraApp", "failed to create directory");
+	            return null;
+	        }
+	    }
+
+	    // Create a media file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    File mediaFile;
+	    if (type == MEDIA_TYPE_IMAGE){
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+	        "IMG_"+ timeStamp + ".jpg");
+	    } else if(type == MEDIA_TYPE_VIDEO) {
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+	        "VID_"+ timeStamp + ".mp4");
+	    } else {
+	        return null;
+	    }
+
+	    return mediaFile;
+	}
 }
